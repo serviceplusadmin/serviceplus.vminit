6e15b8be7c7410b5327db642841f7a701d525eeb
serviceplusadmin <75535689+serviceplusadmin@users.noreply.github.com>
2020-12-10 19:16:56 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+sudo apt-get update -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ sudo apt-key fingerprint 0EBFCD88
+ sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   sudo apt-get update -y
+  sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+   curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+ sudo install minikube-linux-amd64 /usr/local/bin/minikube
+ minikube start --driver=none --insecure-registry=localhost:5000
