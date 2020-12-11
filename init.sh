16169303ba66bb4d50988bb89c8bf76bcfa92573
serviceplusadmin <75535689+serviceplusadmin@users.noreply.github.com>
2020-12-11 09:45:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,25 @@
 #Update System
-sudo apt-get update -y
+apt-get update -y
 #Install Dependencies
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 #Download docker key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 #Register docker fingerprint
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
 #Add docker repo
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 #Update System
-sudo apt-get update -y
+apt-get update -y
+#Fix dpg problem
+dpkg --configure -a
 #Install docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
 #Download minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 #Install minikube
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+install minikube-linux-amd64 /usr/local/bin/minikube
+#Install conntrack
+apt install conntrack -y
 #Initialize & Start minikube
 minikube start --driver=none --insecure-registry=localhost:5000
 #Enable registry on minikube
